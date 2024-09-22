a74ecc7f8dd6fa48685b57fbaae85c73cb2ee771
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-09-22 15:48:41 +0300

    adding the restaurant data

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import footerLogo from "@assets/logo.webp"
+import footerLogo from "@assets/logo (1).jpg"
 const Footer = () => {
     return (
         <footer className="py-16">
@@ -9,7 +9,7 @@ const Footer = () => {
                         <img
                             src={footerLogo}
                             alt="Logo"
-                            className="mx-auto mb-5"
+                            className="mx-auto w-36 h-36 rounded-3xl"
                         />
                     </div>
                 </div>
@@ -17,17 +17,17 @@ const Footer = () => {
                     Lamera Restaurant, Bani Sweif
                 </p>
                 <p className="mb-6">
-                    <a target='_blank' href="https://maps.app.goo.gl/YTgH5WZ5ZyPX5DNw6" className='hover:text-gold'>
-                        3485+M67 Beni Suef
-                    </a>, 010-1000-3323, reservations@example.com
+                    <a target='_blank' href="https://maps.app.goo.gl/UnPiCn5AMuVMSBDb7" className='hover:text-gold'>
+                        3485+R82 Beni Suef
+                    </a>, 01010003321, reservations@example.com
                 </p>
                 <p className="mb-6">
                     Open: 24 Hours
                 </p>
                 <div className="flex justify-center space-x-12 mt-12">
-                    <a target="_blank" href="https://www.facebook.com/lamera.bns" className="hover:text-gray-100 border-b-2 border-gold">Facebook</a>
-                    <a target="_blank" href="https://www.instagram.com/explore/locations/1003752057/" className="hover:text-gray-100 border-b-2 border-gold">Instagram</a>
-                    <a target="_blank" href="https://maps.app.goo.gl/YTgH5WZ5ZyPX5DNw6" className="hover:text-gray-100 border-b-2 border-gold">Location</a>
+                    <a target="_blank" href="https://www.facebook.com/chocobranches" className="hover:text-gray-600 border-b-2 border-gold">Facebook</a>
+                    <a target="_blank" href="https://www.instagram.com/chocobranches/" className="hover:text-gray-600 border-b-2 border-gold">Instagram</a>
+                    <a target="_blank" href="https://maps.app.goo.gl/UnPiCn5AMuVMSBDb7" className="hover:text-gray-600 border-b-2 border-gold">Location</a>
                 </div>
             </div>
         </footer>
